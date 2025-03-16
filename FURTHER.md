7735b6373e7e43d3d7177d3ad91f684bcde1f54f
Ritwik Ranjan Mallik <ritwikranjan.mallik@nutanix.com>
2025-03-16 15:14:34 +0530

    Add refraction

--- FURTHER.md
+++ FURTHER.md
@@ -3,3 +3,5 @@
 2. stb_image.h - https://github.com/nothings/stb and different image formats
 
 3. A Pixel is Not a Little Square - https://www.researchgate.net/publication/244986797
+
+4. Gamma and Gamma Correction
--- camera.h
+++ camera.h
@@ -3,6 +3,7 @@
 
 #include "rtweekend.h"
 #include "hittable.h"
+#include "material.h"
 
 class camera {
     private:
@@ -70,8 +71,11 @@ class camera {
 
             // 0.001 instead of 0 to avoid shadow acne problem
             if (world.hit(r, interval(0.001, infinity), rec)) {
-                vec3 direction = rec.normal + random_unit_vector();
-                return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+                ray scattered;
+                color attenuation;
+                if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+                    return attenuation * ray_color(scattered, depth-1, world);
+                } return color(0, 0, 0);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
