70fd2e1cf60d8b745ddde8cd5afe1149dd985790
Ritwik Ranjan Mallik <ritwikranjan.mallik@nutanix.com>
2025-03-16 08:33:30 +0530

    Add interval

--- sphere.h
+++ sphere.h
@@ -12,7 +12,7 @@ class sphere : public hittable {
     public:
         sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
 
-        bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+        bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = center - r.origin();
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
@@ -27,9 +27,9 @@ class sphere : public hittable {
 
             // Findd the nearest root that lies in the acceptable range
             auto root = (h - sqrtd)/a;
-            if (root <= ray_tmin || ray_tmax <= root) {
-                root = (h + sqrtd)/a;
-                if (root <= ray_tmin || ray_tmax <= root) {
+            if (!ray_t.surrounds(root)) {
+                root = (h + sqrtd) / a;
+                if (!ray_t.surrounds(root)) {
                     return false;
                 }
             }
